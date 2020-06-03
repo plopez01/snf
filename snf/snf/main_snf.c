01ed227d4ab7db266eb489266741e1d23559e03e
Pau Lòpez Pujals <iespaulopez@gmail.com>
2020-06-03 20:09:23 +0200

    Print binary of file

--- snf/snf/main_snf.c
+++ snf/snf/main_snf.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-	printf("%s", argv[argc-1]);
-	return 0;
+
+	//File to compress
+	FILE *fp;
+	errno_t err;
+
+	//File buffer
+	unsigned char buffer[10];
+	
+	//Reads file to compress
+	err = fopen_s(&fp, argv[1], "rb");
+
+	if (err == 0) {
+		size_t fcontent = fread(buffer, sizeof(buffer), 1, fp);
+
+		for (int i = 0; i < sizeof(buffer); i++)
+			printf("%u ", buffer[i]);
+
+
+		return 0;
+	}
+	else {
+		//ERROR
+		return 1;
+	}
+
+	
 }
