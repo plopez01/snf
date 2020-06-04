ff2474b48eed690f1d40abf9471aa6d84b036bc6
Pau Lòpez Pujals <iespaulopez@gmail.com>
2020-06-04 11:04:12 +0200

    Read as text and not binary.

--- snf/snf/main_snf.c
+++ snf/snf/main_snf.c
@@ -7,25 +7,31 @@ int main(int argc, char *argv[]) {
 	FILE *fp;
 	errno_t err;
 	
+	//Dictionary
+	unsigned int dict[20];
 	//Open file to compress
-	err = fopen_s(&fp, argv[1], "rb");
+	err = fopen_s(&fp, argv[1], "r");
 
 	if (err == 0) {
 		
 		//Gets file size
-		fseek(fp, 0L, SEEK_END);
+		fseek(fp, 1, SEEK_END);
 		unsigned int sz = ftell(fp);
 		rewind(fp);
 
 		//Reserves memory to store file data
-		unsigned char* buffer = malloc(sz * sizeof(unsigned char));
+		char* buffer = malloc(sz * sizeof(char));
 
-		//Reads binary data from the file
-		size_t fcontent = fread(buffer, sz, 1, fp);
+		//Reads text data from the file
+		size_t fcontent = fgets(buffer, sz, fp);
 
-		//Prints the binary data
-		for (int i = 0; i < sz; i++)
-			printf("%u ", buffer[i]);
+		printf("%d\n", sz);
+
+		//Prints the text data
+		for (int i = 0; i < sz; i++) {
+
+			printf("%c ", buffer[i]);
+		}		
 
 		//Closes read file
 		fclose(fp);
@@ -36,7 +42,8 @@ int main(int argc, char *argv[]) {
 		return 0;
 	}
 	else {
-		//ERROR
+		//Prints error
+		//TODO: print error details
 		fprintf(stderr, "Cannot open file");
 		return 1;
 	}
