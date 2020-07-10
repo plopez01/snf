1963fd4ff32d6e3d1c7ab8c6a30170a3ee101e5c
Pau Lòpez Pujals <iespaulopez@gmail.com>
2020-07-10 19:28:35 +0200

    Implement DynamicMultidimensionalArray

--- snf/snf/main_snf.c
+++ snf/snf/main_snf.c
@@ -8,6 +8,11 @@ typedef struct {
 	size_t size;
 } DynamicArray;
 
+typedef struct {
+	DynamicArray* array;
+	size_t used;
+	size_t size;
+} MultiDynamicArray;
 
 //Based from https://stackoverflow.com/questions/3536153/c-dynamically-growing-array and adapted to the program
 //TODO: Maybe change system to only reserve necessary memory
@@ -33,24 +38,69 @@ void freeArray(DynamicArray *a) {
 }
 
 
+void initMultiArray(MultiDynamicArray* a, size_t initialSize) {
+	a->array = malloc(initialSize * sizeof(char));
+	a->used = 0;
+	a->size = initialSize;
+}
+
+void insertMultiArray(MultiDynamicArray* a, DynamicArray element) {
+
+	if (a->used == a->size) {
+		a->size *= 2;
+		a->array = realloc(a->array, a->size * sizeof(char));
+	}
+	a->array[a->used++] = element;
+}
+
+void freeMultiArray(MultiDynamicArray* a) {
+	free(a->array);
+	a->array = NULL;
+	a->used = a->size = 0;
+}
+
+
+
 
 
 int main(int argc, char *argv[]) {
-
-
+	
 	//File to compress
 	FILE *fp;
 	errno_t err;
 	
 	//Dynamic array dictionary
 	DynamicArray dict;
+	DynamicArray dict1;
+
+	MultiDynamicArray test;
+	initMultiArray(&test, 4);
+
+
 	initArray(&dict, 1);
+	initArray(&dict1, 1);
+
 
 	//Calculated compression dictionary
 	char str[15] = "Hola que tal";
-	char arr[15][15];
+
+	char arr[5][15];
+
+	int compressed[10];
+
+	char* wordbuffer = malloc(strlen(str) * sizeof(char));
+
+	int buffersize = 0;
 	int wordcount = 0;
 	int charcount = 0;
+	for (int i = 0; i < strlen(str); i++) {
+		if (str[i] == ' ') {
+			
+			buffersize = 0;
+			continue;
+		}
+		buffersize++;
+	}
 	for (int i = 0; i < strlen(str); i++) {
 		if (str[i] == ' ') {
 			wordcount++;
@@ -61,6 +111,49 @@ int main(int argc, char *argv[]) {
 		charcount++;
 		//printf("%c", str[i]);
 	}
+
+	
+
+	insertArray(&dict, 'H');
+	insertArray(&dict, 'o');
+	insertArray(&dict, 'l');
+	insertArray(&dict, 'a');
+
+	insertArray(&dict1, 'M');
+	insertArray(&dict1, 'i');
+	insertArray(&dict1, 'n');
+	insertArray(&dict1, 'e');
+
+	insertMultiArray(&test, dict);
+	insertMultiArray(&test, dict1);
+
+	
+
+	printf("%c", test.array[1].array[0]);
+
+
+	return 0;
+	
+	// Reprogramming Compression
+	for (int i = 0; i < wordcount + 1; i++) {
+		
+		//Check if already on dict
+		for (int x = 0; x < dict.size; x++) {
+			if (strcmp(arr[i], dict.array[x])) {
+				compressed[i] = x;
+				break;
+			}
+		}
+
+		
+		//If not, add to dict and write to compressed array
+
+		//If exist just write to compressed array with correspondent id
+
+	}
+
+
+	//START DEPRECATION
 
 	//Creation of the dictionary
 	for (int i = 0; i < wordcount; i++) {
@@ -76,7 +169,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	//File compression with dictionary
-	static int compressed[15];
+	//static int compressed[15];
 	for (int i = 0; i < wordcount; i++) {
 		for (int j = 0; j < sizeof(dict.array); j++) {
 			if (arr[i] == dict.array[j]) {
@@ -84,6 +177,8 @@ int main(int argc, char *argv[]) {
 			}
 		}
 	}
+
+	//END DEPRECATION
 
 
 	//Open file to compress
